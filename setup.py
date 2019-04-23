2ac0cd3da6093854fe2e78f0fcb0a4657ce84bd3
shijiaa <seveny1997@hotmail.com>
2019-04-23 02:13:06 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -20,6 +20,6 @@ setup(name='lipnet',
 		'tensorflow-gpu==1.0.1',
 		'Theano==0.9.0',
         'nltk==3.2.2',
-        'sk-video==1.1.7',
+        'sk-video==1.1.10',
         'dlib==19.4.0'
     ])
